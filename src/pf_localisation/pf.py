d705f3ebb179a4b7f47606e99e9ac92f4e9c0c9c
lzy199808 <83507733+lzy199808@users.noreply.github.com>
2021-10-27 15:51:51 +0100

    Update pf.py

--- src/pf_localisation/pf.py
+++ src/pf_localisation/pf.py
@@ -18,20 +18,21 @@ class PFLocaliser(PFLocaliserBase):
         super(PFLocaliser, self).__init__()
         
         # ----- Set motion model parameters
+
         self.ROTATION_NOISE = np.random.uniform(0.05, 0.2)
         self.TRANSLATION_NOISE = np.random.uniform(0.05, 0.2)
         self.DRIFT_NOISE = np.random.uniform(0.05, 0.2)
 
         # ----- Set sensor model parameters
-        self.PARTICLE_COUNT = 200
+
         self.NUMBER_PREDICTED_READINGS = 20
+        self.PARTICLE_COUNT = 200
 
         # ----- Set noise parameters
-        self.PARTICLE_POSITIONAL_NOISE = np.random.uniform(75, 100)
-        self.PARTICLE_ANGULAR_NOISE = np.random.uniform(1, 120)
 
+        self.PARTICLE_POS_NOISE = np.random.uniform(60, 100)
+        self.PARTICLE_ANG_NOISE = np.random.uniform(1, 135)
 
-  
     def initialise_particle_cloud(self, initial_pose):
         """
         Called whenever an initial_pose message is received (to change the
@@ -48,20 +49,19 @@ class PFLocaliser(PFLocaliserBase):
         for _ in range(self.PARTICLE_COUNT):
             pose = Pose()
 
-            positional_noise_x = random.gauss(0, self.PARTICLE_POSITIONAL_NOISE) * self.TRANSLATION_NOISE
-            positional_noise_y = random.gauss(0, self.PARTICLE_POSITIONAL_NOISE) * self.DRIFT_NOISE
+            positional_noise_x = random.gauss(0, self.PARTICLE_POS_NOISE) * self.TRANSLATION_NOISE
+            positional_noise_y = random.gauss(0, self.PARTICLE_POS_NOISE) * self.DRIFT_NOISE
 
             pose.position.x = initial_pose.pose.pose.position.x + positional_noise_x
             pose.position.y = initial_pose.pose.pose.position.y + positional_noise_y
 
-            ANGULAR_NOISE = (random.vonmisesvariate(0, self.PARTICLE_ANGULAR_NOISE) - math.pi) * self.ROTATION_NOISE
+            angular_noise = (random.vonmisesvariate(0, self.PARTICLE_ANG_NOISE) - math.pi) * self.ROTATION_NOISE
 
-            pose.orientation = rotateQuaternion(initial_pose.pose.pose.orientation, ANGULAR_NOISE)
+            pose.orientation = rotateQuaternion(initial_pose.pose.pose.orientation, angular_noise)
 
             pose_array.poses.append(pose)
             
         return pose_array
- 
 
     def update_particle_cloud(self, scan):
         """
@@ -104,64 +104,51 @@ class PFLocaliser(PFLocaliserBase):
             pose_array.poses.append(copy.deepcopy(self.particlecloud.poses[i - 1]))
 
         # In order to converge to the new pose, adjust the noise parameters
+
+        noise = []
+        noise[0] = self.ROTATION_NOISE
+        noise[1] = self.TRANSLATION_NOISE
+        noise[2] = self.DRIFT_NOISE
+        noise[3] = self.PARTICLE_POS_NOISE
+        noise[4] = self.PARTICLE_ANG_NOISE
+        noise[0] = noise[0] - 0.1 if noise[0] > 0.2 else np.random.uniform(0.05, 0.2)
+        noise[1] = noise[1] - 0.1 if noise[1] > 0.2 else np.random.uniform(0.05, 0.2)
+        noise[2] = noise[2] - 0.1 if noise[2] > 0.2 else np.random.uniform(0.05, 0.2)
+        noise[3] = noise[3] - 0.1 if noise[3] > 0.2 else np.random.uniform(0.05, 0.2)
+        noise[4] = noise[4] - 1 if noise[4] > 90.0 else np.random.uniform(0.05, 90)
+        # print(noise)
+
         for poses in pose_array.poses:
-            if self.ROTATION_NOISE > 0.2:
-                self.ROTATION_NOISE -= 0.05
-
-            else:
-                self.ROTATION_NOISE = np.random.uniform(0.05, 0.2)
-
-            if self.TRANSLATION_NOISE > 0.2:
-                self.TRANSLATION_NOISE -= 0.05
-
-            else:
-                self.TRANSLATION_NOISE = np.random.uniform(0.05, 0.2)
-
-            if self.DRIFT_NOISE > 0.2:
-                self.DRIFT_NOISE -= 0.05
-
-            else:
-                self.DRIFT_NOISE = np.random.uniform(0.05, 0.2)
-
-            if self.PARTICLE_POSITIONAL_NOISE > 2.0:
-                self.PARTICLE_POSITIONAL_NOISE -= 0.1
-
-            else:
-                self.PARTICLE_POSITIONAL_NOISE = np.random.uniform(0.05, 2)
-
-            if self.PARTICLE_ANGULAR_NOISE > 90.0:
-                self.PARTICLE_ANGULAR_NOISE -= 1.0
-
-            else:
-                self.PARTICLE_ANGULAR_NOISE = np.random.uniform(0.05, 90)
-
             # Adds position noise to the x and y coordinates
-            poses.position.x += random.gauss(0, self.PARTICLE_POSITIONAL_NOISE) * self.TRANSLATION_NOISE
-            poses.position.y += random.gauss(0, self.PARTICLE_POSITIONAL_NOISE) * self.DRIFT_NOISE
+
+            poses.position.x += random.gauss(0, noise[3]) * noise[1]
+            poses.position.y += random.gauss(0, noise[3]) * self.noise[2]
 
             # Add Angular noise
-            ANGULAR_NOISE = (random.vonmisesvariate(0, self.PARTICLE_ANGULAR_NOISE) - math.pi) * self.ROTATION_NOISE
+
+            angular_noise = (random.vonmisesvariate(0, noise[4]) - math.pi) * noise[0]
 
             # Add orientation noise
-            poses.orientation = rotateQuaternion(poses.orientation, ANGULAR_NOISE)
-        
+
+            poses.orientation = rotateQuaternion(poses.orientation, angular_noise)
+
+        # print(pose_array)
         self.particlecloud = pose_array
-        """
-        Output the estimated position and orientation of the robot
-        """
-        Robot_estimated_position = self.estimatedpose.pose.pose.position
-        Robot_estimated_orientation = self.estimatedpose.pose.pose.orientation
+
+        # Output the estimated position and orientation of the robot
+
+        robot_estimated_position = self.estimatedpose.pose.pose.position
+        robot_estimated_orientation = self.estimatedpose.pose.pose.orientation
     
         # Transformation
-        Robot_Quaternion = [Robot_estimated_orientation.x, Robot_estimated_orientation.y, Robot_estimated_orientation.z, Robot_estimated_orientation.w]
-        roll, pitch, yaw = euler_from_quaternion(Robot_Quaternion)
+
+        robot_quaternion = [robot_estimated_orientation.x, robot_estimated_orientation.y, robot_estimated_orientation.z, robot_estimated_orientation.w]
+        roll, pitch, yaw = euler_from_quaternion(robot_quaternion)
 
         # Output the position and orientation estimates of the robot
-        print("/Robot estimated Pose: Robot Position: [{x:.2f}, {y:.2f}]    Robot Orientation: [{yaw:.2f}]".format(
-                                                                                                    x=Robot_estimated_position.x,
-                                                                                                    y=Robot_estimated_position.y,
-                                                                                                    yaw=math.degrees(yaw)))
 
+        print("/Robot estimated Pose: Robot Position: [{x:.2f}, {y:.2f}]    Robot Orientation: [{yaw:.2f}]".format(
+            x=robot_estimated_position.x, y=robot_estimated_position.y, yaw=math.degrees(yaw)))
 
     def estimate_pose(self):
         """
@@ -171,29 +158,33 @@ class PFLocaliser(PFLocaliserBase):
         :Return:
             | (geometry_msgs.msg.Pose) robot's estimated pose.
         """
-        # Set the clustering distance threshold
-        CLUSTER_DISTANCE_THRESHOLD = 0.3
+
         """
         # Mean pose
         estimated_pose = Pose() 
 
         position_sum_x, position_sum_y, orientation_sum_z, orientation_sum_w = (0 for _ in range(4))
         
-        for pose_object in self.particlecloud.poses:
-            position_sum_x    += pose_object.position.x 
-            position_sum_y    += pose_object.position.y 
-            orientation_sum_z += pose_object.orientation.z 
-            orientation_sum_w += pose_object.orientation.w 
+        for poses in self.particlecloud.poses:
+            position_sum_x += poses.position.x 
+            position_sum_y += poses.position.y 
+            orientation_sum_z += poses.orientation.z 
+            orientation_sum_w += poses.orientation.w 
 
-        estimated_pose.position.x    = position_sum_x    / self.PARTICLE_COUNT 
-        estimated_pose.position.y    = position_sum_y    / self.PARTICLE_COUNT 
+        estimated_pose.position.x = position_sum_x    / self.PARTICLE_COUNT 
+        estimated_pose.position.y = position_sum_y    / self.PARTICLE_COUNT 
         estimated_pose.orientation.z = orientation_sum_z / self.PARTICLE_COUNT 
         estimated_pose.orientation.w = orientation_sum_w / self.PARTICLE_COUNT    
         """
         # Hierarchical agglomerate clustering
+
+        # Set the clustering distance threshold
+
+        cluster_distance_threshold = 0.3
         estimated_pose = Pose()
 
         # Initialize position and direction array variables
+
         position_x, position_y, orientation_z, orientation_w = ([] for _ in range(4))
 
         for pose in self.particlecloud.poses:
@@ -202,66 +193,65 @@ class PFLocaliser(PFLocaliserBase):
             orientation_z.append(pose.orientation.z)
             orientation_w.append(pose.orientation.w)
 
-        position_x    = np.array(position_x)
-        position_y    = np.array(position_y)
+        position_x = np.array(position_x)
+        position_y = np.array(position_y)
         orientation_z = np.array(orientation_z)
         orientation_w = np.array(orientation_w)
 
         # form a distance matrix
-        distance_matrix = np.column_stack((position_x, position_y, orientation_z, orientation_w))
 
-        linkage_matrix = linkage(distance_matrix, method='median')
+        dis_matrix = np.column_stack((position_x, position_y, orientation_z, orientation_w))
 
-        particle_cluster_identities = fcluster(linkage_matrix, CLUSTER_DISTANCE_THRESHOLD, criterion='distance')
-        #print(particle_cluster_identities)
+        link_matrix = linkage(dis_matrix, method='median')
 
-        cluster_count = max(particle_cluster_identities)
-        cluster_particle_counts = [0] * cluster_count
-        cluster_probability_weight_sums = [0] * cluster_count
+        cluster_characteristic = fcluster(link_matrix, cluster_distance_threshold, criterion='distance')
 
-        for i, particle_cluster_identity in enumerate(particle_cluster_identities):
+        cluster_count = max(cluster_characteristic)
+        cluster_counts = [0] * cluster_count
+        cluster_weight_sums = [0] * cluster_count
+
+        for i, cluster_characteristic in enumerate(cluster_characteristic):
             pose = self.particlecloud.poses[i]
 
-            probability_weight = self.sensor_model.get_weight(latest_scan, pose)
+            cluster_weight = self.sensor_model.get_weight(latest_scan, pose)
 
-            cluster_particle_counts[particle_cluster_identity - 1] += 1
-            cluster_probability_weight_sums[particle_cluster_identity - 1] += probability_weight
+            cluster_counts[cluster_characteristic - 1] += 1
+            cluster_weight_sums[cluster_characteristic - 1] += cluster_weight
 
         # find the most accurate clusters of particles overall
-        cluster_highest_belief = cluster_probability_weight_sums.index(max(cluster_probability_weight_sums)) + 1
-        cluster_highest_belief_particle_count = cluster_particle_counts[cluster_highest_belief - 1]
+
+        cluster_highest_weight = cluster_weight_sums.index(max(cluster_weight_sums)) + 1
+        cluster_highest_weight_count = cluster_counts[cluster_highest_weight - 1]
 
         # Initializes the position and orientation
+
         position_sum_x, position_sum_y, orientation_sum_z, orientation_sum_w = (0 for _ in range(4))
-
-        for i, particle_cluster_identity in enumerate(particle_cluster_identities):
-            if (particle_cluster_identity == cluster_highest_belief):
+        for i, cluster_characteristic in enumerate(cluster_characteristic):
+            if cluster_characteristic == cluster_highest_weight:
                 pose = self.particlecloud.poses[i]
-
-                position_sum_x    += pose.position.x
-                position_sum_y    += pose.position.y
+                position_sum_x += pose.position.x
+                position_sum_y += pose.position.y
                 orientation_sum_z += pose.orientation.z
                 orientation_sum_w += pose.orientation.w
 
-        estimated_pose.position.x    = position_sum_x    / cluster_highest_belief_particle_count
-        estimated_pose.position.y    = position_sum_y    / cluster_highest_belief_particle_count
-        estimated_pose.orientation.z = orientation_sum_z / cluster_highest_belief_particle_count
-        estimated_pose.orientation.w = orientation_sum_w / cluster_highest_belief_particle_count
+        estimated_pose.position.x = position_sum_x / cluster_highest_weight_count
+        estimated_pose.position.y = position_sum_y / cluster_highest_weight_count
+        estimated_pose.orientation.z = orientation_sum_z / cluster_highest_weight_count
+        estimated_pose.orientation.w = orientation_sum_w / cluster_highest_weight_count
 
-        """
-        Output the estimated position and orientation of the robot
-        """
+        # Output the estimated position and orientation of the robot
+
         robot_estimated_position = estimated_pose.position
         robot_estimated_orientation = estimated_pose.orientation
 
         # Transformation
-        Robot_Quaternion = [robot_estimated_orientation.x, robot_estimated_orientation.y, robot_estimated_orientation.z, robot_estimated_orientation.w]
-        roll, pitch, yaw = euler_from_quaternion(Robot_Quaternion)
+
+        robot_quaternion = [robot_estimated_orientation.x, robot_estimated_orientation.y,
+                            robot_estimated_orientation.z, robot_estimated_orientation.w]
+        roll, pitch, yaw = euler_from_quaternion(robot_quaternion)
 
         # Output the position and orientation estimates of the robot
         print("Robot Position: [{x:.2f}, {y:.2f}]    Robot Orientation: [{yaw:.2f}]".format(
-                                                                                                    x=robot_estimated_position.x, 
-                                                                                                    y=robot_estimated_position.y, 
-                                                                                                    yaw=math.degrees(yaw)))
+            x=robot_estimated_position.x, y=robot_estimated_position.y, yaw=math.degrees(yaw)))
 
         return estimated_pose
